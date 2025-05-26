3837cb7222ba95d46198f430b1237d9c68b2e370
3l0i_Kot <arsenghj@mail.ru>
2025-05-26 09:08:35 +0500

    7.5
    Добавление поста

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -7,11 +7,19 @@ import java.util.*;
 
 @Service
 public class PostService {
-    public List<Post> listAllPosts() {
-        List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
+
+    {
         posts.add(new Post("Это первый пост! Привет!", new Date()));
         posts.add(new Post("Я сдал комиссию по ООП!", new Date()));
         posts.add(new Post("Как дела?", new Date()));
+    }
+
+    public List<Post> listAllPosts() {
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
