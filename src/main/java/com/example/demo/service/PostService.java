b4e830fa05f97350c2c320dcb5b7b8f1bbbd02db
3l0i_Kot <arsenghj@mail.ru>
2025-06-02 01:15:43 +0500

    9.2
    Добавлена база даннх (патч)

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,25 +1,24 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PostService {
-    private List<Post> posts = new ArrayList<>();
-
-    {
-        posts.add(new Post((long) posts.size(), "Это первый пост! Привет!", new Date()));
-        posts.add(new Post((long) posts.size(), "Я сдал комиссию по ООП!", new Date()));
-        posts.add(new Post((long) posts.size(), "Как дела?", new Date()));
-    }
+    @Autowired
+    PostRepository postRepository;
 
     public List<Post> listAllPosts() {
-        return posts;
+        return StreamSupport.stream(postRepository.findAll().spliterator(), false).toList();
     }
 
     public void create(String text) {
-        posts.add(new Post((long) posts.size(),text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
